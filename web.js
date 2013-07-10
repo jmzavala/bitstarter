8e85019e8d58bb78a1e7ea44d6c6b1f35204be64
Juan Manuel Zavala <jmzavala.olv@gmail.com>
2013-07-10 03:38:39 +0000

    add html file in web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ 
+  var bf = fs.readFileSync('index.html','utf-8');
+  response.send(bf.to_string());
 });
 
 var port = process.env.PORT || 5000;
